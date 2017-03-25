1060ac327cf8956a5d1f3c0f8479b3f885f59bd2
ghigbie <gehigbie@gmail.com>
2017-03-25 18:27:16 +0000

    add basic style

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const Campground = require("./models/campground"),
 mongoose.connect("mongodb://localhost/yelp_camp_v4");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
 seedDB();
 
 
